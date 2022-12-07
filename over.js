247afd124c7b391541032317514ce2ac7b8d5525
Camilo <camicardenasp@gmail.com>
2022-12-07 12:29:21 -0500

    Ahora si funciona la música del final

--- over.js
+++ over.js
@@ -1,4 +1,5 @@
-document.getElementById("html").onload = function() {myFunction()};
+const body  = document.querySelector("body")
+body.onload = function() {myFunction()};
 
 function myFunction() {
     const funebre = new Audio('/sounds/marcha.mp3');
